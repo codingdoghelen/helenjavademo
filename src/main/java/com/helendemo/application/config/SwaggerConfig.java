9875ec18602493bf3eae44b342f95c9cfc08ea5b
codingdoghelen <codinghk2020@gmail.com>
2022-03-09 18:51:31 +0800

    fix: api spec

--- src/main/java/com/helendemo/application/config/SwaggerConfig.java
+++ src/main/java/com/helendemo/application/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.helendemo.application.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -10,25 +11,52 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Collections;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-
-    private ApiInfo commonInfo() {
+     private ApiInfo commonInfo() {
         return new ApiInfoBuilder()
                 .title("Rest Api")
                 .version("1.0")
                 .build();
     }
+    //    @Bean
+//    public Docket allApi() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .groupName("INTERNAL")
+//                .useDefaultResponseMessages(false)
+//                .select()
+//                .apis(RequestHandlerSelectors.any())
+//                .paths(PathSelectors.any())
+//                .build()
+//                .apiInfo(commonInfo());
+//    }
 
     @Bean
     public Docket allApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("USER")
+                .protocols(Collections.singleton("https"))
+                .groupName("EXTERNAL")
                 .useDefaultResponseMessages(false)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(commonInfo());
+    }
+
+
+    @Bean
+    public Docket userApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .protocols(Collections.singleton("https"))
+                .groupName("INTERNAL")
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.regex("/api/user/.*"))
                 .build()
                 .apiInfo(commonInfo());
     }
