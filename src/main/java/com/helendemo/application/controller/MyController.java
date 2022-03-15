bb81ff1ecbc5be765506740e0b143f2b67721e0c
codingdoghelen <codinghk2020@gmail.com>
2022-03-15 15:03:39 +0800

    remove: testing comment

--- src/main/java/com/helendemo/application/controller/MyController.java
+++ src/main/java/com/helendemo/application/controller/MyController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// TESTING
-
 @RestController
 @RequestMapping("/api")
 @Api(value = "HelloWorld Resource")
